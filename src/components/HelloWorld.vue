7a6a479312fe0ba208d20cfe1fb999f55400a180
Roberto Mirabella <bronzetto@outlook.com>
2019-06-02 19:14:54 +0100

    added styles folder

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,96 +1,33 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
-          target="_blank"
-          rel="noopener"
-          >unit-jest</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="container">
+    <div class="row">
+      <div
+        class="box box1 col-lg-3 col-md-6 px-5"
+      >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat optio eaque praesentium suscipit unde deserunt, maiores fugiat tenetur. In corrupti nesciunt pariatur ipsam maxime. Laudantium vel commodi fugiat quod delectus.</div>
+      <div
+        class="box box2 col-lg-3 col-md-6 py-5"
+      >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat optio eaque praesentium suscipit unde deserunt, maiores fugiat tenetur. In corrupti nesciunt pariatur ipsam maxime. Laudantium vel commodi fugiat quod delectus.</div>
+      <div
+        class="box box3 col-lg-3 col-md-6 px-5"
+      >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat optio eaque praesentium suscipit unde deserunt, maiores fugiat tenetur. In corrupti nesciunt pariatur ipsam maxime. Laudantium vel commodi fugiat quod delectus.</div>
+      <div
+        class="box box4 col-lg-3 col-md-6 p-5"
+      >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat optio eaque praesentium suscipit unde deserunt, maiores fugiat tenetur. In corrupti nesciunt pariatur ipsam maxime. Laudantium vel commodi fugiat quod delectus.</div>
+    </div>
+
+    <hr>
+
+    <div
+      class="cont d-flex flex-xs-row flex-sm-column flex-md-row flex-lg-column justify-content-between align-items-center w-100"
+    >
+      <div class="boxS">Random Box</div>
+      <div class="boxS">Random Box</div>
+      <div class="boxS">Random Box</div>
+      <div class="boxS">Random Box</div>
+      <div class="boxS">Random Box</div>
+      <div class="boxS">Random Box</div>
+      <div class="boxS">Random Box</div>
+    </div>
   </div>
 </template>
 
@@ -105,18 +42,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+@import "~@/styles/components/_helloWorld";
 </style>
